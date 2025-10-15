599637a8abbd34ede7c4c255ecf9aa98ea1eb223
Kushnir-Daniil <kn1b24.kushnir@kpnu.edu.ua>
2025-10-15 09:15:50 +0300

    Add files via upload

---
+++ ООП_Лаб_роб_№1_KN1_B24_Кушнір.cs
@@ -0,0 +1,19 @@
+using System;
+class HelloWorld {
+	static void Main() {
+	    Console.Write("Введіть два цілих Числа\n");
+		int a = int.Parse(Console.ReadLine());
+		int b = int.Parse(Console.ReadLine());
+        Console.Write("Множення\n");
+		Console.WriteLine(a*b);
+		Console.Write("Середнє арифметичне\n");
+		Console.WriteLine((a+b)/2);
+        Console.Write("Числа Фібоначі\n");
+		int end = int.Parse(Console.ReadLine());
+		int j=1;
+		for (int i = 1; i <= end; i += j) {
+			Console.Write("{0} ", i);
+			j = i - j;
+		}
+	}
+}
